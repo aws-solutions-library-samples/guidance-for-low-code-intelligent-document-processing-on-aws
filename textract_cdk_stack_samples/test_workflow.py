054ea25714c4701ea3e8a86228521c595db24567
schadem <45048633+schadem@users.noreply.github.com>
2023-06-02 16:01:14 -0700

    fix workshop for cannot import name 'DEFAULT_CIPHERS' from 'urllib3.util.ssl issue

--- textract_cdk_stack_samples/test_workflow.py
+++ textract_cdk_stack_samples/test_workflow.py
@@ -1,6 +1,5 @@
 from constructs import Construct
-import os
-from aws_cdk import (CfnOutput, RemovalPolicy, Stack, Duration)
+from aws_cdk import (Stack)
 import amazon_textract_idp_cdk_constructs as tcdk
 
 
@@ -9,7 +8,7 @@ class TestWorkflow(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        configurator_task = tcdk.TextractClassificationConfigurator(
+        tcdk.TextractClassificationConfigurator(
             self,
             f"Configurator",
         )

2522503d94515d25419b72043271c6a299f87334
Rafael Koike <koiker@amazon.com>
2024-04-16 17:41:10 -0400

    Updated the workflow to run with multiple CM APIs

--- textract_cdk_stack_samples/textract_comprehend_medical_workflow.py
+++ textract_cdk_stack_samples/textract_comprehend_medical_workflow.py
@@ -21,8 +21,6 @@ class TextractComprehendMedicalWorkflow(Stack):
             **kwargs)
 
         script_location = os.path.dirname(__file__)
-        s3_upload_prefix = "uploads"
-        s3_output_prefix = "textract-output"
 
         # BEWARE! This is a demo/POC setup, remove the auto_delete_objects=True and
         document_bucket = s3.Bucket(self,
@@ -30,85 +28,91 @@ class TextractComprehendMedicalWorkflow(Stack):
                                     auto_delete_objects=True,
                                     removal_policy=RemovalPolicy.DESTROY)
         s3_output_bucket = document_bucket.bucket_name
-        s3_temp_output_prefix = "temp"
-        workflow_name = "TextractComprehendMedical"
+        comprehend_medical_operations = ['ICD10', 'RXNORM', 'SNOMEDCT', 'DETECT_ENTITIES_V2', 'DETECT_PHI']
 
-        decider_task = tcdk.TextractPOCDecider(
-            self,
-            f"{workflow_name}-Decider",
-        )
+        for cm_task in comprehend_medical_operations:
+            s3_upload_prefix = f'uploads-{cm_task.lower()}'
+            s3_output_prefix = f'textract-output-{cm_task.lower()}'
+            s3_temp_output_prefix = f'temp-{cm_task.lower()}'
+            workflow_name = f'TextractComprehendMedical-{cm_task.lower()}'
 
-        textract_async_task = tcdk.TextractGenericAsyncSfnTask(
-            self,
-            "TextractAsync",
-            s3_output_bucket=s3_output_bucket,
-            s3_temp_output_prefix=s3_temp_output_prefix,
-            integration_pattern=sfn.IntegrationPattern.WAIT_FOR_TASK_TOKEN,
-            lambda_log_level="DEBUG",
-            timeout=Duration.hours(24),
-            input=sfn.TaskInput.from_object({
-                "Token":
-                sfn.JsonPath.task_token,
-                "ExecutionId":
-                sfn.JsonPath.string_at('$$.Execution.Id'),
-                "Payload":
-                sfn.JsonPath.entire_payload,
-            }),
-            result_path="$.textract_result")
+            decider_task = tcdk.TextractPOCDecider(
+                self,
+                f"{workflow_name}-Decider",
+            )
 
-        textract_async_to_json = tcdk.TextractAsyncToJSON(
-            self,
-            "TextractAsyncToJSON2",
-            s3_output_prefix=s3_output_prefix,
-            s3_output_bucket=s3_output_bucket)
+            textract_async_task = tcdk.TextractGenericAsyncSfnTask(
+                self,
+                "TextractAsync",
+                s3_output_bucket=s3_output_bucket,
+                s3_temp_output_prefix=s3_temp_output_prefix,
+                integration_pattern=sfn.IntegrationPattern.WAIT_FOR_TASK_TOKEN,
+                lambda_log_level="DEBUG",
+                timeout=Duration.hours(24),
+                input=sfn.TaskInput.from_object({
+                    "Token":
+                    sfn.JsonPath.task_token,
+                    "ExecutionId":
+                    sfn.JsonPath.string_at('$$.Execution.Id'),
+                    "Payload":
+                    sfn.JsonPath.entire_payload,
+                }),
+                result_path="$.textract_result")
 
-        comprehendmedical_task = tcdk.TextractComprehendMedical(
-            self,
-            "ComprehendMedical",comprehendMedicalJobType="ICD10")
+            textract_async_to_json = tcdk.TextractAsyncToJSON(
+                self,
+                "TextractAsyncToJSON2",
+                s3_output_prefix=s3_output_prefix,
+                s3_output_bucket=s3_output_bucket)
 
-        workflow_chain = sfn.Chain \
-            .start(decider_task) \
-            .next(textract_async_task) \
-            .next(textract_async_to_json) \
-            .next(comprehendmedical_task)
+            comprehendmedical_task = tcdk.TextractComprehendMedical(
+                self,
+                "ComprehendMedical",
+                comprehendMedicalJobType=cm_task)
 
-        # GENERIC
-        state_machine = sfn.StateMachine(self,
-                                         workflow_name,
-                                         definition=workflow_chain)
+            workflow_chain = sfn.Chain \
+                .start(decider_task) \
+                .next(textract_async_task) \
+                .next(textract_async_to_json) \
+                .next(comprehendmedical_task)
 
-        lambda_step_start_step_function = lambda_.DockerImageFunction(
-            self,
-            "LambdaStartStepFunctionGeneric",
-            code=lambda_.DockerImageCode.from_image_asset(
-                os.path.join(script_location, '../lambda/startstepfunction')),
-            memory_size=128,
-            architecture=lambda_.Architecture.X86_64,
-            environment={"STATE_MACHINE_ARN": state_machine.state_machine_arn})
+            # GENERIC
+            state_machine = sfn.StateMachine(self,
+                                             workflow_name,
+                                             definition=workflow_chain)
 
-        lambda_step_start_step_function.add_to_role_policy(
-            iam.PolicyStatement(actions=['states:StartExecution'],
-                                resources=[state_machine.state_machine_arn]))
+            lambda_step_start_step_function = lambda_.DockerImageFunction(
+                self,
+                "LambdaStartStepFunctionGeneric",
+                code=lambda_.DockerImageCode.from_image_asset(
+                    os.path.join(script_location, '../lambda/startstepfunction')),
+                memory_size=128,
+                architecture=lambda_.Architecture.X86_64,
+                environment={"STATE_MACHINE_ARN": state_machine.state_machine_arn})
 
-        document_bucket.add_event_notification(
-            s3.EventType.OBJECT_CREATED,
-            s3n.LambdaDestination(
-                lambda_step_start_step_function),  #type: ignore
-            s3.NotificationKeyFilter(prefix=s3_upload_prefix))
+            lambda_step_start_step_function.add_to_role_policy(
+                iam.PolicyStatement(actions=['states:StartExecution'],
+                                    resources=[state_machine.state_machine_arn]))
 
-        # OUTPUT
-        CfnOutput(
-            self,
-            "DocumentUploadLocation",
-            value=f"s3://{document_bucket.bucket_name}/{s3_upload_prefix}/")
-        CfnOutput(
-            self,
-            "StartStepFunctionLambdaLogGroup",
-            value=lambda_step_start_step_function.log_group.log_group_name)
-        current_region = Stack.of(self).region
-        CfnOutput(
-            self,
-            'StepFunctionFlowLink',
-            value=
-            f"https://{current_region}.console.aws.amazon.com/states/home?region={current_region}#/statemachines/view/{state_machine.state_machine_arn}"
-        )
+            document_bucket.add_event_notification(
+                s3.EventType.OBJECT_CREATED,
+                s3n.LambdaDestination(
+                    lambda_step_start_step_function),  #type: ignore
+                s3.NotificationKeyFilter(prefix=s3_upload_prefix))
+
+            # OUTPUT
+            CfnOutput(
+                self,
+                "DocumentUploadLocation",
+                value=f"s3://{document_bucket.bucket_name}/{s3_upload_prefix}/")
+            CfnOutput(
+                self,
+                "StartStepFunctionLambdaLogGroup",
+                value=lambda_step_start_step_function.log_group.log_group_name)
+            current_region = Stack.of(self).region
+            CfnOutput(
+                self,
+                'StepFunctionFlowLink',
+                value=
+                f"https://{current_region}.console.aws.amazon.com/states/home?region={current_region}#/statemachines/view/{state_machine.state_machine_arn}"
+            )
